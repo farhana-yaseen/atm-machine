6944f4d4edf57665754b92845e9bcf037dfc8916
farhana <farhanayaseen2@gmail.com>
2024-03-27 05:36:54 -0500

    create-atm-machine

--- index.js
+++ index.js
@@ -7,24 +7,42 @@ const codeInput = await inquirer.prompt({
     message: "Please Enter your Pin Code : "
 });
 if (codeInput.userCode === myPinCode) {
-    console.log("correct pin code!!!");
-    const operation = await inquirer.prompt({
-        name: "userChoice",
-        type: "list",
-        message: "please select option",
-        choices: ["withdraw", "blance check"]
-    });
-    if (operation.userChoice === "withdraw") {
-        const amount = await inquirer.prompt({
-            name: "userAmount",
+    console.log("valid pin code!!!");
+    const accountType = await inquirer.prompt([{
+            name: "account",
+            type: "list",
+            message: "please select account type",
+            choices: ["saving", "current"]
+        },
+        {
+            name: "transaction",
+            type: "list",
+            message: "please select your transaction",
+            choices: ["withdraw", "fast cash", "balance check"]
+        }]);
+    if (accountType.transaction === "withdraw") {
+        const userAmount = await inquirer.prompt({
+            name: "amount",
+            type: "number",
+            message: "Enter your amount ",
+        });
+        myBalnce -= userAmount.amount;
+        console.log(`"your remaining balance is "${myBalnce}`);
+    }
+    if (myBalnce <= accountType.amount) {
+        console.log(" insufficient amount ");
+    }
+    if (accountType.transaction === "fast cash") {
+        const selectAmount = await inquirer.prompt({
+            name: "userChoice",
             type: "list",
             message: "Please select your amount ",
             choices: ["1000", "2000", "5000", "10000"]
         });
-        myBalnce -= amount.userAmount;
+        myBalnce -= selectAmount.userChoice;
         console.log(`"your remaining balance is "${myBalnce}`);
     }
-    if (codeInput.userCode === "balance check") {
+    if (accountType.transaction === "balance check") {
         console.log(`"your current balance is " ${myBalnce}`);
     }
 }
--- index.ts
+++ index.ts
@@ -11,37 +11,58 @@ const codeInput = await inquirer.prompt(
     })
     
     if (codeInput.userCode === myPinCode){
-        console.log("correct pin code!!!")
-       
-        const operation = await inquirer.prompt(
-            {
-                name: "userChoice",
+        console.log("valid pin code!!!")
+
+        const accountType = await inquirer.prompt(
+           [ {
+                name: "account",
                 type: "list",
-                message: "please select option",
-                choices: ["withdraw" , "blance check"]
-            })
-
-            if (operation.userChoice === "withdraw"){
-                const amount = await inquirer.prompt(
-                    {
-                        name: "userAmount",
-                        type: "list",
-                        message: "Please select your amount ",
-                        choices: ["1000","2000","5000","10000"]
-            
-                    })
-
-                    myBalnce -= amount.userAmount
-                    console.log(`"your remaining balance is "${myBalnce}` )
-
-                 } if (codeInput.userCode === "balance check"){
-                        console.log(`"your current balance is " ${myBalnce}` )
-            
-    } 
+                message: "please select account type",
+                choices: ["saving" , "current"]
+            },
        
+            {
+                name: "transaction",
+                type: "list",
+                message: "please select your transaction",
+                choices: ["withdraw" , "fast cash", "balance check"]
+            }])
 
-    }else{
-        console.log("incorrect pin code!!!")
-    }
+            if (accountType.transaction === "withdraw"){
 
-    
+                const userAmount = await inquirer.prompt(
+                    {
+                        name: "amount",
+                        type: "number",
+                        message: "Enter your amount ",
+                                    
+                    })
+                    myBalnce -= userAmount.amount
+                    console.log(`"your remaining balance is "${myBalnce}`); 
+                    
+                 } 
+                
+                 if (accountType.transaction === "fast cash"){
+
+                    const selectAmount = await inquirer.prompt(
+                        {
+                            name: "userChoice",
+                            type: "list",
+                            message: "Please select your amount ",
+                            choices: ["1000","2000","5000","10000"]
+                
+                        })
+                        myBalnce -= selectAmount.userChoice
+                    console.log(`"your remaining balance is "${myBalnce}`);
+
+                    }       
+                    
+                    if (accountType.transaction === "balance check"){
+                        console.log(`"your current balance is " ${myBalnce}` );    
+                    } 
+                   
+                    } else {
+                    console.log("incorrect pin code!!!")
+                    
+                }
+            
