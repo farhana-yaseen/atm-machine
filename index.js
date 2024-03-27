148d461e9848c112ed88102d90227a35687f481c
farhana-yaseen <164941355+farhana-yaseen@users.noreply.github.com>
2024-03-27 00:55:27 +0000

    create-atm

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+let myBalnce = 150000;
+let myPinCode = 223344;
+const codeInput = await inquirer.prompt({
+    name: "userCode",
+    type: "number",
+    message: "Please Enter your Pin Code : "
+});
+if (codeInput.userCode === myPinCode) {
+    console.log("correct pin code!!!");
+    const operation = await inquirer.prompt({
+        name: "userChoice",
+        type: "list",
+        message: "please select option",
+        choices: ["withdraw", "blance check"]
+    });
+    if (operation.userChoice === "withdraw") {
+        const amount = await inquirer.prompt({
+            name: "userAmount",
+            type: "number",
+            message: "Enter your amount ",
+        });
+        myBalnce -= amount.userAmount;
+        console.log(`"your remaining balance is "${myBalnce}`);
+    }
+    if (codeInput.userCode === "balance check") {
+        console.log(`"your current balance is " ${myBalnce}`);
+    }
+}
+else {
+    console.log("incorrect pin code!!!");
+}
